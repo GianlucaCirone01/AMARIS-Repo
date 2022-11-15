88ad0841f5239340f0927bff21e02677a3c2e74f
Luca Rolando <luca.rolando@amaris.com>
2022-11-15 16:34:40 +0100

    flyway fixed

--- paypal-user-service/src/main/java/com/amaris/it/paypal/user/model/User.java
+++ paypal-user-service/src/main/java/com/amaris/it/paypal/user/model/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   @Column(unique = true)
   private String username;
