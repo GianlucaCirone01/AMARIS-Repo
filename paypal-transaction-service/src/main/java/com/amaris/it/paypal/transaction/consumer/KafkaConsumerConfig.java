089e53305a429de06bc341d50edf0940affb7665
AlbertoAinstano <grecoalberto32@gmail.com>
2022-11-17 18:28:25 +0100

    Fixato problema kafka

--- paypal-transaction-service/src/main/java/com/amaris/it/paypal/transaction/consumer/KafkaConsumerConfig.java
+++ paypal-transaction-service/src/main/java/com/amaris/it/paypal/transaction/consumer/KafkaConsumerConfig.java
@@ -1,15 +1,14 @@
 package com.amaris.it.paypal.transaction.consumer;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
@@ -23,13 +22,13 @@ public class KafkaConsumerConfig {
   @Value(value = "${kafka.bootstrapAddress}")
   private String bootstrapAddress;
 
-  @Bean
-  public ProducerFactory<String, Object> consumerConfig() {
-    Map<String, Object> configProps = new HashMap<>();
-    configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-    configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    return new DefaultKafkaProducerFactory<>(configProps);
+  Map<String, Object> consumerConfigs() {
+    Map<String, Object> props = new HashMap<>();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    return props;
   }
 
 
