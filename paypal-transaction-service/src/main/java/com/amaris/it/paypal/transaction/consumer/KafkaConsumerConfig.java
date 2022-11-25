674cbd89056058c9e9f3433ee730fb7de3e3d4ea
AlbertoAinstano <grecoalberto32@gmail.com>
2022-11-25 15:31:33 +0100

    KafkaConsumerConfig take object

--- paypal-transaction-service/src/main/java/com/amaris/it/paypal/transaction/consumer/KafkaConsumerConfig.java
+++ paypal-transaction-service/src/main/java/com/amaris/it/paypal/transaction/consumer/KafkaConsumerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,20 +24,20 @@ public class KafkaConsumerConfig {
   private String bootstrapAddress;
 
   @Bean
-  public ConsumerFactory<String, String> consumerFactory() {
+  public ConsumerFactory<String, Object> consumerFactory() {
     Map<String, Object> config = new HashMap<>();
 
     config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
     config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
     config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
     return new DefaultKafkaConsumerFactory<>(config);
   }
 
   @Bean
   public ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory() {
-    ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
     factory.setConsumerFactory(consumerFactory());
     return factory;
   }
